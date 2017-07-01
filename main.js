800ef23453eebc384cb7d8b0abcd24f6bcbe5e39
Brian Hummel <frankgucci@mac.com>
2017-07-01 15:21:27 -0500

    cleaned up spacing, tabs

--- main.js
+++ main.js
@@ -4,231 +4,227 @@ var inquirer = require('inquirer');
 var fs = require('fs');
 
 var askQuestion = function() {
-    inquirer.prompt([
-      {
-        type: "list",
-        name: "doWhat",
-        message: "What would you like to do ?",
-        choices: ["Practice with a flashcard deck", "Create New Flashcards"]
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "doWhat",
+      message: "What would you like to do ?",
+      choices: ["Practice with a flashcard deck", "Create New Flashcards"]
+    },
+    {
+      type: "list",
+      message: "Which type of card(s) do you want to create?",
+      name: "cardType",
+      when: function(answers){return answers.doWhat === "Create New Flashcards";},
+      choices: ["Create Basic", "Create Cloze"]   
+    },
+    { 
+      type: "list",
+      message: "Practice with which flashcard deck?",
+      name: "cardType",
+      when: function(answers){return answers.doWhat === "Practice with a flashcard deck";},
+      choices: ["Basic Deck", "Cloze Deck"]   
+    }
+  ]).then(function(answers) {
+    var action = answers.cardType;
+    // used to set date information when writing to log.txt
+    var currentdate = new Date();   
+    var lookup = {
+      // text to be written as log entry header---------------------------------
+      logTime: "Log entry created on " + currentdate.getDate() + "/" + (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds(),
+      // if cloze statement is not contained in full text, throw/log error------
+      logError: function(){
+        console.log("cloze not contained in text, please try again: \n");
+        fs.appendFile("log.txt", lookup.logTime + "\ncloze not contained in text\n", function(error){
+          if(error)console.log("error");
+          else lookup[action]();
+        });
       },
-      {
-        type: "list",
-        message: "Which type of card(s) do you want to create?",
-        name: "cardType",
-        when: function(answers){return answers.doWhat === "Create New Flashcards";},
-        choices: ["Create Basic", "Create Cloze"]   
-      },
-      { 
-        type: "list",
-        message: "Practice with which flashcard deck?",
-        name: "cardType",
-        when: function(answers){return answers.doWhat === "Practice with a flashcard deck";},
-        choices: ["Basic Deck", "Cloze Deck"]   
-      }
-    ]).then(function(answers) {
-        var action = answers.cardType;
-        // used to set date information when writing to log.txt
-        var currentdate = new Date();   
-        var lookup = {
-            // text to be written as log entry header
-            logTime: "Log entry created on " + currentdate.getDate() + "/" + (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds(),
-            // if cloze statement is not contained in full text, throw/log error
-            logError: function(){
-                console.log("cloze not contained in text, please try again: \n");
-                fs.appendFile("log.txt", lookup.logTime + "\ncloze not contained in text\n", function(error){
-                        if(error)console.log("error");
-                        else lookup[action]();
-                    });
-            },
-            // create basic flashcards------------------------------------
-            "Create Basic": function() {
+      // create basic flashcards------------------------------------------------
+      "Create Basic": function() {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Enter the question text",
+            name: "questionText"
+          },
+          {
+            type: "input",
+            message: "Enter the anwswer text",
+            name: "answerText"
+          }
+        ]).then(function(answers) {
+          fs.readFile('./basic.json', 'utf-8', function(err, data) {
+            if (err) throw err;
+            var arrayOfObjects = JSON.parse(data);
+            arrayOfObjects.basicDeck.push(new BasicCard(answers.questionText.toUpperCase(), answers.answerText.toUpperCase()));
+            fs.writeFile('./basic.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+              if (err) throw err;
+            });
+          });
+          setTimeout(function(){
+            inquirer.prompt([
+              {
+                type: "list",    
+                message: "Add another card?",
+                choices: ["Yes", "No"],
+                name: "addAnother"
+              }
+            ]).then(function(answers) {
+              if (answers.addAnother === "Yes"){
+                lookup["Create Basic"]();
+              }
+              else if (answers.addAnother === "No"){
                 inquirer.prompt([
-                    {
-                        type: "input",
-                        message: "Enter the question text",
-                        name: "questionText"
-                    },
-                    {
-                        type: "input",
-                        message: "Enter the anwswer text",
-                        name: "answerText"
-                    }
-                    ]).then(function(answers) {
-                      fs.readFile('./basic.json', 'utf-8', function(err, data) {
-                          if (err) throw err;
-                          var arrayOfObjects = JSON.parse(data);
-                          arrayOfObjects.basicDeck.push(new BasicCard(answers.questionText.toUpperCase(), answers.answerText.toUpperCase()));
-                          fs.writeFile('./basic.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
-                              if (err) throw err;
-                          });
-                      });
-                      setTimeout(function(){
-                          inquirer.prompt([
-                              {
-                                  type: "list",    
-                                  message: "Add another card?",
-                                  choices: ["Yes", "No"],
-                                  name: "addAnother"
-                              }
-                              ]).then(function(answers) {
-                                  if (answers.addAnother === "Yes"){
-                                      lookup["Create Basic"]();
-                                  }
-                                  else if (answers.addAnother === "No"){
-                                      inquirer.prompt([
-                                        {
-                                            type: "list",    
-                                            message: "Would you like to practice with this deck?",
-                                            choices: ["Yes", "No"],
-                                            name: "practiceNow"
-                                        }
-                                        ]).then(function(answers) {
-                                            if (answers.practiceNow === "Yes"){
-                                                lookup["Basic Deck"]();
-                                            }
-                                            else if (answers.practiceNow === "No"){
-                                                return false;
-                                            }
-                                        });
-                                  }
-                              });
-                      }, 500);
-                    });
-            },
-            // create cloze flashcards------------------------------------
-            "Create Cloze": function(){
-                inquirer.prompt([
-                    {
-                        type: "input",  
-                        message: "Enter full text of statement",
-                        name: "fullText",
-                    },
-                    {
-                        type: "input",
-                        message: "Enter portion of text to be cloze deleted",
-                        name: "clozeText"
-                    }
+                  {
+                    type: "list",    
+                    message: "Would you like to practice with this deck?",
+                    choices: ["Yes", "No"],
+                    name: "practiceNow"
+                  }
                 ]).then(function(answers) {
-                    if ((answers.fullText).includes(answers.clozeText)){
-                        fs.readFile('./cloze.json', 'utf-8', function(err, data) {
-                            if (err) throw err;
-                            var arrayOfObjects = JSON.parse(data);
-                            arrayOfObjects.clozeDeck.push(new ClozeCard(answers.fullText.toUpperCase(), answers.clozeText.toUpperCase()));
-                            fs.writeFile('./cloze.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
-                                if (err) throw err;
-                            });
-                        });
-                        setTimeout(function(){
-                            inquirer.prompt([
-                                {
-                                    type: "list",    
-                                    message: "Add another card?",
-                                    choices: ["Yes", "No"],
-                                    name: "addAnother"
-                                }
-                                ]).then(function(answers) {
-                                    if (answers.addAnother === "Yes"){
-                                        lookup["Create Cloze"]();
-                                    }
-                                    else if (answers.addAnother === "No"){
-                                        return false;
-                                    }
-                                });
-                        }, 500);
-                    }
-                    else lookup.logError();
+                  if (answers.practiceNow === "Yes"){
+                    lookup["Basic Deck"]();
+                  }
+                  else if (answers.practiceNow === "No"){
+                    return false;
+                  }
                 });
-            },
-            // practice: read from basic deck----------------------------
-            "Basic Deck": function() {
-                console.log("Basic Deck Practice");
-                fs.readFile('./basic.json', 'utf-8', function(err, data) {
+              }
+            });                                       
+          }, 500);
+        });
+      },
+      // create cloze flashcards------------------------------------------------
+      "Create Cloze": function(){
+        inquirer.prompt([
+          {
+            type: "input",  
+            message: "Enter full text of statement",
+            name: "fullText",
+          },
+          {
+            type: "input",
+            message: "Enter portion of text to be cloze deleted",
+            name: "clozeText"
+          }
+        ]).then(function(answers) {
+            if ((answers.fullText).includes(answers.clozeText)){
+              fs.readFile('./cloze.json', 'utf-8', function(err, data) {
+                if (err) throw err;
+                var arrayOfObjects = JSON.parse(data);
+                arrayOfObjects.clozeDeck.push(new ClozeCard(answers.fullText.toUpperCase(), answers.clozeText.toUpperCase()));
+                fs.writeFile('./cloze.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
                     if (err) throw err;
-                    var arrayOfObjects = JSON.parse(data);
-                    var count = 0;
-                    play();
-                    function play(){
-                        var tries = 0;
-                        if (count < arrayOfObjects.basicDeck.length){
-                            inquirer.prompt([
-                                {
-                                    type: "input",
-                                    message: arrayOfObjects.basicDeck[count].question,
-                                    name: "question"
-                                }
-                                ]).then(function(answers) {
-                                    if (answers.question.toUpperCase() === arrayOfObjects.basicDeck[count].answer){
-                                        console.log("Correct!");
-                                        count++;
-                                        play();
-                                    }
-                                    else {
-                                        if (tries < 1){
-                                            console.log("Incorrect, please try again");
-                                            tries++;
-                                            play();
-                                        }
-                                        else {
-                                            console.log("Incorrect! \nThe correct answer is: " + arrayOfObjects.basicDeck[count].answer);
-                                            count++;
-                                            play();
-                                        }
-                                    }
-                                });
-                        }
-                        else console.log("End of deck");
-                    }     
                 });
-
-
-
-
-            },
-            // practice: read from cloze deck-----------------------------
-            "Cloze Deck": function(){
-                console.log("Cloze Deck Practice");
-                fs.readFile('./cloze.json', 'utf-8', function(err, data) {
-                    if (err) throw err;
-                    var arrayOfObjects = JSON.parse(data);
-                    var count = 0;
-                    play();
-                    function play(){
-                        var tries = 0;
-                        if (count < arrayOfObjects.clozeDeck.length){
-                            inquirer.prompt([
-                                {
-                                    type: "input",
-                                    message: arrayOfObjects.clozeDeck[count].partial,
-                                    name: "question"
-                                }
-                                ]).then(function(answers) {
-                                    if (answers.question.toUpperCase() === arrayOfObjects.clozeDeck[count].cloze){
-                                        console.log("Correct!");
-                                        count++;
-                                        play();
-                                    }
-                                    else {
-                                        if (tries < 1){
-                                            console.log("Incorrect, please try again");
-                                            tries++;
-                                            play();
-                                        }
-                                        else {
-                                            console.log("Incorrect! \nThe correct answer is: " + arrayOfObjects.clozeDeck[count].cloze);
-                                            count++;
-                                            play();
-                                        }
-                                    }
-                                });
-                        }
-                        else console.log("End of deck");
-                    }   
+              });
+              setTimeout(function(){
+                inquirer.prompt([
+                  {
+                    type: "list",    
+                    message: "Add another card?",
+                    choices: ["Yes", "No"],
+                    name: "addAnother"
+                  }
+                ]).then(function(answers) {
+                  if (answers.addAnother === "Yes"){
+                    lookup["Create Cloze"]();
+                  }
+                  else if (answers.addAnother === "No"){
+                    return false;
+                  }
                 });
+              }, 500);
+          }
+          else lookup.logError();
+        });
+      },
+      // practice: read from basic deck-----------------------------------------
+      "Basic Deck": function() {
+        console.log("Basic Deck Practice");
+        fs.readFile('./basic.json', 'utf-8', function(err, data) {
+          if (err) throw err;
+          var arrayOfObjects = JSON.parse(data);
+          var count = 0;
+          play();
+          function play(){
+            var tries = 0;
+            if (count < arrayOfObjects.basicDeck.length){
+              inquirer.prompt([
+                {
+                  type: "input",
+                  message: arrayOfObjects.basicDeck[count].question,
+                  name: "question"
+                }
+              ]).then(function(answers) {
+                if (answers.question.toUpperCase() === arrayOfObjects.basicDeck[count].answer){
+                  console.log("Correct!");
+                  count++;
+                  play();
+                }
+                else {
+                  if (tries < 1){
+                    console.log("Incorrect, please try again");
+                    tries++;
+                    play();
+                  }
+                  else {
+                    console.log("Incorrect! \nThe correct answer is: " + arrayOfObjects.basicDeck[count].answer);
+                    count++;
+                    play();
+                  }
+                }
+              });
             }
-        };
-        // uses the answers from the INQUIRER section to call a function from the LOOKUP object 
-        lookup[action]();   
-    });
-};        
+            else console.log("End of deck");
+          }     
+        });
+      },
+      // practice: read from cloze deck-----------------------------------------
+      "Cloze Deck": function(){
+        console.log("Cloze Deck Practice");
+        fs.readFile('./cloze.json', 'utf-8', function(err, data) {
+          if (err) throw err;
+          var arrayOfObjects = JSON.parse(data);
+          var count = 0;
+          play();
+          function play(){
+            var tries = 0;
+            if (count < arrayOfObjects.clozeDeck.length){
+              inquirer.prompt([
+                {
+                  type: "input",
+                  message: arrayOfObjects.clozeDeck[count].partial,
+                  name: "question"
+                }
+              ]).then(function(answers) {
+                if (answers.question.toUpperCase() === arrayOfObjects.clozeDeck[count].cloze){
+                  console.log("Correct!");
+                  count++;
+                  play();
+                }
+                else {
+                  if (tries < 1){
+                    console.log("Incorrect, please try again");
+                    tries++;
+                    play();
+                  }
+                  else {
+                    console.log("Incorrect! \nThe correct answer is: " + arrayOfObjects.clozeDeck[count].cloze);
+                    count++;
+                    play();
+                  }
+                }
+              });
+            }
+            else console.log("End of deck");
+          }   
+        });
+      }
+    };
+    // uses the answers from the INQUIRER section to call a function from the LOOKUP object 
+    lookup[action]();           
+  });
+};  
 // initializes the app - INQUIRER prompt
 askQuestion();
