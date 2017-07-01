cb327855457aa9190e62ecb82e0f21da36476762
Brian Hummel <frankgucci@mac.com>
2017-07-01 14:53:03 -0500

    converted both basic and cloze constructors to be scope-safe

--- BasicCard.js
+++ BasicCard.js
@@ -1,6 +1,8 @@
-var basicDeck = [];
-
+// scope-safe constructor
 function BasicCard(question, answer){
+    if(!(this instanceof BasicCard)){
+        return new BasicCard(question, answer);
+    }
     this.question = question;
     this.answer = answer;
 }
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,11 @@
-var ClozeCard = function(fullText, clozeText){
+// scope-safe constructor
+function ClozeCard(fullText, clozeText){
+     if(!(this instanceof ClozeCard)){
+        return new ClozeCard(fullText, clozeText);
+    }
     this.fullText = fullText;
     this.cloze = clozeText;
     this.partial = fullText.replace(clozeText, "...");
-};
+}
 
 module.exports = ClozeCard;
